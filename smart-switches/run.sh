1b8534a6ebf67444968e42a07e8a0b5257998649
lucaspopp0 <lucaspopp0@gmail.com>
2025-04-23 16:15:36 -0700

    print config on startup

--- smart-switches/run.sh
+++ smart-switches/run.sh
@@ -4,10 +4,8 @@ export PATH="/usr/local/go/bin:$PATH"
 
 env
 
-go version
-
 SERVER_PORT=8000
 
 echo "Starting server on ${SERVER_PORT}"
 
-python3 -m http.server "$SERVER_PORT"
+curl -X GET -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" http://supervisor/core/api/config
