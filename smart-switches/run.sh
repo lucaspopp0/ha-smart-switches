97989fae065517d9672faa4463fef992e01c9582
lucaspopp0 <lucaspopp0@gmail.com>
2025-04-23 21:51:08 -0700

    use executable

--- smart-switches/run.sh
+++ smart-switches/run.sh
@@ -7,7 +7,4 @@ export PATH="/usr/local/go/bin:$PATH"
 SWITCHES_JSON=/data/switches.json
 SITE_DIR=/smartswitches/site
 
-cd /smartswitches/server
-
-go get ./...
-go run main.go
+/smartswitches/server/server
