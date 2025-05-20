1da5e358e149a126b6206fadf530bcd7434ac4e4
lucaspopp0 <lucaspopp0@gmail.com>
2025-05-20 16:21:26 -0700

    resort buttons

--- smart-switches/site/src/components/editors/layout.tsx
+++ smart-switches/site/src/components/editors/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ConfirmModal from '../modals/confirm';
 import { CaretRightFilled } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Switch } from 'antd';
 import { config } from 'process';
 import ExecutablePicker from '../inputs/executable-picker';
 import { DefaultApi, Config, Layouts } from '../../api';
@@ -38,46 +38,102 @@ const LayoutEditor: React.FC<LayoutEditorProps> = props => {
         return <div style={styles.flexRow} />
     }
 
+    const hasWheelRoutines = ButtonsByLayout[props.currentLayout].includes('wheel-routines');
+    const canFlip = ButtonsByLayout[props.currentLayout].includes('flipped');
+
+    const basicButtons = ButtonsByLayout[props.currentLayout]
+      .filter(button => button != 'flipped' && button != 'wheel-routines')
+      .sort((a, b): number => {
+        // Handle the equal condition right away
+        if (a == b) {
+          return 0
+        }
+
+        let intA = parseInt(a)
+        let aIsInt = !Number.isNaN(intA)
+
+        let intB = parseInt(b)
+        let bIsInt = !Number.isNaN(intB)
+
+        // Remember, at this point elements will not be equal
+        if (bIsInt && aIsInt) {
+          return intA < intB ? -1 : 1
+        } else if (aIsInt) {
+          return 1
+        } else if (bIsInt) {
+          return -1
+        } else {
+          return a < b ? -1 : 1
+        }
+      })
+
+    const row = (key: string, title: string, accessory: React.ReactNode) => (
+      <div key={key} style={{ display: 'flex', padding: 12, }}>
+        {title}
+        <div style={{ display: 'flex', height: 0, flexGrow: 2, }} />
+        {accessory}
+      </div>
+    )
+
     return (
         <div style={styles.flexRow}>
           <div style={styles.flexRow} />
           <div style={{ display: 'flex', flexDirection: 'column', width: 400 }}>
-            {ButtonsByLayout[props.currentLayout].map(buttonName => (
-              <div key={buttonName} style={{ display: 'flex', padding: 12, }}>
-                {buttonName}
-                <div style={{ display: 'flex', height: 0, flexGrow: 2, }} />
+            {basicButtons.map(buttonName => (
+              row(buttonName, buttonName, <>
                 <ExecutablePicker
-                  value={currentLayout[buttonName as keyof typeof currentLayout]}
-                  api={props.api}
-                  onPick={async picked => {
-                    currentLayout[buttonName as keyof typeof currentLayout] = picked?.entityId
-                    currentSwitch.layouts[props.currentLayout as LayoutKey] = currentLayout
-                    
-                    props.onUpdate({
+                    value={currentLayout[buttonName as keyof typeof currentLayout]}
+                    api={props.api}
+                    onPick={async picked => {
+                      currentLayout[buttonName as keyof typeof currentLayout] = picked?.entityId
+                      currentSwitch.layouts[props.currentLayout as LayoutKey] = currentLayout
+                      
+                      props.onUpdate({
+                          switches: {
+                              ...(props.config?.switches ?? {}),
+                              [props.currentSwitch as string]: currentSwitch,
+                          }
+                      })
+                    }}
+                  />
+                  <Button
+                    icon={<CaretRightFilled />}
+                    disabled={!currentLayout[buttonName as keyof typeof currentLayout]}
+                    onClick={() => {
+                      props.api
+                        .press({
+                          _switch: props.currentSwitch as string,
+                          layout: props.currentLayout as string,
+                          key: buttonName,
+                        })
+                        .catch(err => {
+                          console.error(err)
+                        })
+                    }}
+                  />
+                </>,
+              )
+            ))}
+            {canFlip
+              ? row('flipped', 'Flipped?', <Switch
+                  value={'flipped' in currentLayout ? currentLayout.flipped : false}
+                  onChange={checked => {
+                    if ('flipped' in currentLayout) {
+                      currentLayout.flipped = checked
+
+                      currentSwitch.layouts[props.currentLayout as LayoutKey] = currentLayout
+
+                      props.onUpdate({
                         switches: {
                             ...(props.config?.switches ?? {}),
                             [props.currentSwitch as string]: currentSwitch,
                         }
-                    })
-                  }}
-                />
-                <Button
-                  icon={<CaretRightFilled />}
-                  disabled={!currentLayout[buttonName as keyof typeof currentLayout]}
-                  onClick={() => {
-                    props.api
-                      .press({
-                        _switch: props.currentSwitch as string,
-                        layout: props.currentLayout as string,
-                        key: buttonName,
                       })
-                      .catch(err => {
-                        console.error(err)
-                      })
-                  }}
-                />
-                </div>
-            ))}
+                    }
+                  }} 
+                />)
+              : <></>
+            }
             <div style={{ display: 'flex', flexGrow: 2, }}/>
           </div>
         </div>
